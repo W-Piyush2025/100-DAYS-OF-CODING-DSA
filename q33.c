67b4d83daf8d1b0213318673d70b150630182822
W-Piyush2025 <legendpiyush2006@gmail.com>
2026-03-05 23:52:17 +0530

    Infix to Postfix Conversion

---
+++ q33.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    if (top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int priority(char x) {
+    if (x == '+' || x == '-')
+        return 1;
+    if (x == '*' || x == '/')
+        return 2;
+    if (x == '^')
+        return 3;
+    return 0;
+}
+
+int main() {
+    char exp[MAX];
+    char *e, x;
+
+    printf("Enter infix expression: ");
+    scanf("%s", exp);
+
+    e = exp;
+
+    while (*e != '\0') {
+        
+        if (isalnum(*e)) {     // operand
+            printf("%c", *e);
+        }
+        
+        else if (*e == '(') {
+            push(*e);
+        }
+        
+        else if (*e == ')') {
+            while ((x = pop()) != '(')
+                printf("%c", x);
+        }
+        
+        else {   // operator
+            while (top != -1 && priority(stack[top]) >= priority(*e))
+                printf("%c", pop());
+            push(*e);
+        }
+
+        e++;
+    }
+
+    while (top != -1)
+        printf("%c", pop());
+
+    return 0;
+}
